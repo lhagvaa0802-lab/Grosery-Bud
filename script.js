da324709fedd4fb1ddadbb1b12565579fb937904
lhagvaa0802-lab <lhagvaa0802@gmail.com>
2025-12-18 08:15:33 -0500

    add

--- script.js
+++ script.js
@@ -1,86 +1,60 @@
 const inputBox = document.querySelector(".input-group input");
 const submitBtn = document.querySelector(".input-group button");
-const itemList = document.querySelector("#item-list");
+const itemListEl = document.querySelector("#item-list");
 const clearButton = document.getElementById("clearBtn");
-console.log(clearBtn);
-console.log(itemList);
 
 let items = [];
 let itemId = 1;
+
+
+
 const submit = () => {
-  const itemList = inputBox.value;
-  const item = {
-    id: itemId,
-    text: itemList,
-    isChecked: false,
-  };
-
-  console.log(itemList);
-  items.push(item);
-  console.log(items);
-  renderItems(items);
-  clearInput();
-  itemId++;
-};
-
-const renderItems = (taskArr) => {
-  let conItems = "";
-
-  taskArr.forEach((item) => {
-    const conItem = createItem(item);
-    conItems += conItem;
-  });
-
-  itemList.innerHTML = conItems;
-};
-
-const createItem = (item) => {
-  return ` <div class="box">
-          <p>${item.text}</p>
-          <div>
-            <input type="checkbox" id="${item.id}" ${item.isChecked && "checked"} onchange="updateItems(${item.id})"/>
-            <button   onclick="deleteBtn(${item.id})">Delete</button>
-          </div>
-        </div>`;
-};
-
-const clearInput = () => {
-  inputBox.value = "";
-};
-const updateItems = (id) => {
-  const updatedItems = items.map((item) => {
-    if (item.id === id) {
-      return { ...item, isChecked: !item.isChecked };
-     
-    } else {
-      return item;
-    }
-  });
-  console.log(updatedItems)
-
-  items = updatedItems;
-  renderItems(updatedItems);
-};
+  const text = inputBox.value
+  if (!text) return;
 
 
-const deleteBtn = (Id) => {
 
-  const deletedItems = items.filter((item) => !item.isChecked===true );
-  items = deletedItems;
   
-  renderItems(deletedItems)
-}
+  const item = { id: itemId, text: text };
+  items.push(item);
+  itemId++;
+  inputBox.value = "";
+  renderItems();
+};
 
-const clear=()=>{
 
-   const clearedItems = items.filter((item) => !item.isChecked === true);
-   items = clearedItems;
+const renderItems = () => {
+  let html = "";
+  items.forEach((item) => {
+    html += createItem(item);
+  });
+  itemListEl.innerHTML = html;
+};
 
-   renderItems(clearedItems);
-}
 
+const createItem = (item) => `
+  <div class="box">
+    <p>${item.text}</p>
+    <div>
+      <button onclick="startEdit(${item.id})">Edit</button>
+      <button onclick="deleteItem(${item.id})">Delete</button>
+    </div>
+  </div>
+`;
+
+
+
+const deleteItem = (id) => {
+  items = items.filter((item) => item.id !== id);
+  renderItems();
+};
+
+
+const clear = () => {
+  items = [];
+  renderItems();
+};
 
 
 submitBtn.addEventListener("click", submit);
 clearButton.addEventListener("click", clear);
-console.log(items);
