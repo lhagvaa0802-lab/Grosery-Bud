e98e1099cc36c027d5d5bad1f7b2a4790f1e2730
lhagvaa0802-lab <lhagvaa0802@gmail.com>
2025-12-25 11:27:06 +0800

    add

--- script.js
+++ script.js
@@ -2,64 +2,77 @@ const inputBox = document.querySelector(".input-group input");
 const submitBtn = document.querySelector(".input-group button");
 const itemListEl = document.querySelector("#item-list");
 const clearButton = document.getElementById("clearBtn");
+const saveBtn = document.getElementById("save_btn");
 
 let items = [];
 let itemId = 1;
 
-
-
 const submit = () => {
-  const text = inputBox.value
+  const text = inputBox.value;
   if (!text) return;
 
   const item = { id: itemId, text: text };
-  
+
   items.push(item);
- console.log(items);
+  console.log(items);
   itemId++;
   inputBox.value = "";
   renderItems(items);
 };
 
-
 const renderItems = (itemsArr) => {
   let html = "";
- itemsArr.forEach((item) => {
-   html += createItem(item);
-   console.log(html);
- });
+  itemsArr.forEach((item) => {
+    html += createItem(item);
+    console.log(html);
+  });
   itemListEl.innerHTML = html;
 };
-
 
 const createItem = (item) => `
   <div class="box">
     <p>${item.text}</p>
     <div class=buttons>
-      <button class=edit-btn onclick="startEdit(${item.id})">Edit</button>
-      <button class=delete-btn onclick="deleteItem(${item.id})">Delete</button>
+     
+      <i class="fa-solid fa-pen-to-square" onclick="startEdit(${item.id})"></i>
+      <i class="fa-regular fa-trash-can" onclick="deleteItem(${item.id})"></i>
+      
     </div>
   </div>
 `;
-
-
 
 const deleteItem = (id) => {
   items = items.filter((item) => item.id !== id);
   renderItems(items);
 };
 
-
 const clear = () => {
   items = [];
   renderItems(items);
 };
-const startEdit = (id) =>{
-     const item = items.find((item) => item.id === id);
-     inputBox.value = item.text;
-     deleteItem(id)
-}
 
+const startEdit = (id) => {
+  console.log(id);
+  const item = items.find((item) => item.id === id);
+  inputBox.value = item.text;
+  submitBtn.style.display = "none";
+  saveBtn.style.display = "block";
+};
+
+const save = (id) => {
+  console.log(id);
+  const updateItems = items.map((item) => {
+    if (item.id === id) {
+      return { ...item, text: inputBox.value };
+    } else {
+      return item;
+    }
+  });
+  console.log(inputBox.value);
+  items = updateItems;
+  renderItems(updateItems);
+};
 
 submitBtn.addEventListener("click", submit);
 clearButton.addEventListener("click", clear);
+saveBtn.addEventListener("click", save);
